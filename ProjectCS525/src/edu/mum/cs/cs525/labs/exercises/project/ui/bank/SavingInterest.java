8fd78c5540a017a1521b5384063aac2b2181ae02
amaraa0327 <amaraa0327@gmail.com>
2017-10-24 15:51:43 -0500

    add all file

---
+++ ProjectCS525/src/edu/mum/cs/cs525/labs/exercises/project/ui/bank/SavingInterest.java
@@ -0,0 +1,17 @@
+package edu.mum.cs.cs525.labs.exercises.project.ui.bank;
+
+import edu.mum.cs.cs525.labs.exercises.project.framework.InterestType;
+
+public class SavingInterest implements InterestType{
+
+	public double getMinimumPayment() {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+	public double calcInterest(double pBalance) {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+}
